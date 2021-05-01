8907e0a8c7a8e208b33e342638b0d5cc50c073f2
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-05-01 01:34:42 -0300

    feat: listar um pokemon por id | GET

--- src/main/java/com/alps/pokedex/controller/PokemonController.java
+++ src/main/java/com/alps/pokedex/controller/PokemonController.java
@@ -3,10 +3,11 @@ package com.alps.pokedex.controller;
 import com.alps.pokedex.model.Pokemon;
 import com.alps.pokedex.repository.PokedexRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/pokemons")
@@ -18,6 +19,13 @@ public class PokemonController {
     @GetMapping
     public Flux<Pokemon> getAllPokemons() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Pokemon>> getPokemon(@PathVariable String id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
 
